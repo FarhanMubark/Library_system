2c72dcf5b3a562618f8d28e8843a1879d7ddfde7
Farhan Alanazi <87990702+FarhanMubark@users.noreply.github.com>
2023-08-17 18:09:40 +0300

    Update LibrarinRepository.java

--- main/java/com/example/library_system/Repository/LibrarinRepository.java
+++ main/java/com/example/library_system/Repository/LibrarinRepository.java
@@ -2,6 +2,7 @@ package com.example.library_system.Repository;
 
 import com.example.library_system.Model.Librarian;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,7 +10,11 @@ public interface LibrarinRepository extends JpaRepository<Librarian, Integer> {
 
     Librarian findLibrarianById(Integer id);
 
+    @Query("select l from Librarian l where l.username=?1 and l.password=?2")
     Librarian findLibrarianByUsernameAndPassword(String username, String password);
 
+//    @Query(" select l from Librarian l where l.email=?1")
+//    Librarian findLibrarianByEmail(String email);
 
+    Librarian findLibrarianByEmail(String email);
 }
